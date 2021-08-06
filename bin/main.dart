27e142b7413c8db578d83c6fbd668265888deb3f
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-08-05 23:26:58 -0400

    Test action

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,52 @@
 import 'dart:io';
 
-void main(List<String> args) {
-	final version = args[0];
-	var url = 'https://github.com/vlang/v/archive/refs/tags/';
+import 'package:http/http.dart';
 
-	if (version.startsWith('weekly')) {
-		url += version + '.tar.gz';
-		
-	} else if (version.length == 40) {
-		// It's a commit hash
-		
-	} else {
+void main(List<String> args) async {
+	args = ['0.2.1'];
+  final version = args[0];
+  // final os = Platform.environment['RUNNER_OS'];
+	final os = 'Linux';
+	final arm = 'false';
+  var url = 'https://github.com/vlang/v';
 
-	}
+  try {
+    assert(os != null && os.isNotEmpty);
+  } catch (e) {
+    throw Exception('Can\'t detect runner OS!');
+  }
+
+  if (version.length == 40) {
+    // It's a commit hash
+		throw Exception('Currently not supporting commit hash!');
+  } else {
+		url += '/archive/refs/tags/';
+
+    url += version;
+
+		switch (os) {
+			case 'Linux':
+				url += '/v_linux';
+				break;
+			case 'Windows':
+				url += '/v_windows';
+				break;
+			case 'macOS':
+				url += '/v_macos';
+				break;
+			default:
+		}
+
+		if (arm == 'true') {
+			url += '_arm64';
+		}
+
+		url += '.zip';
+
+		final request = await HttpClient().getUrl(Uri.parse(url));
+		final response = await request.close();
+		await response.pipe(File('v.zip').openWrite());
+		
+		print('File downloaded!');
+  }
 }
